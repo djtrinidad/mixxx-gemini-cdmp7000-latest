0c1fc24b0f5588e982ef501d8846a834b3c25e2a
djtrinidad <djtrinidad@users.noreply.github.com>
2024-02-22 16:01:50 -0600

    Update Gemini_CDMP-7000-test-scripts.js

--- Gemini_CDMP-7000-test-scripts.js
+++ Gemini_CDMP-7000-test-scripts.js
@@ -3,6 +3,8 @@ var CDMP7000 = {};
 CDMP7000.init = function() {
   CDMP7000.leftDeck = new CDMP7000.Deck(1, 1);
 
+  CDMP7000.leftDeck.reconnectComponents();
+
 };
 
 CDMP7000.shutdown = function() {
@@ -14,14 +16,17 @@ CDMP7000.Deck = function (deckNumbers, midiChannel) {
  /* this.playButton = new components.PlayButton([0x90 + midiChannel, 0x01]); */
   this.cueButton = new components.CueButton([0x90, 0x01]);
   this.playButton = new components.PlayButton([0x90, 0x02]);
-}
 
-this.reconnectComponents(function (c) {
+  this.reconnectComponents(function (c) {
         if (c.group === undefined) {
             // 'this' inside a function passed to reconnectComponents refers to the ComponentContainer
             // so 'this' refers to the custom Deck object being constructed
             c.group = this.currentDeck;
         }
     });
+  
+}
+
+
 
 CDMP7000.Deck.prototype = new components.Deck();
