0bd58b9f5172549ebcb4b453f1047b46cccedf27
djtrinidad <djtrinidad@users.noreply.github.com>
2024-02-24 10:01:40 -0600

    Update Gemini_CDMP-7000-test-scripts.js

--- Gemini_CDMP-7000-test-scripts.js
+++ Gemini_CDMP-7000-test-scripts.js
@@ -29,7 +29,7 @@ CDMP7000.Deck = function (deckNumbers, midiChannel) {
     }
  
   this.memoButtonPressed = function (value) {
-    if ((value == 0x90) && (CDMP7000.memoActive == false)) {
+    if ((value == 0x7f) && (CDMP7000.memoActive == false)) {
       CDMP7000.memoActive = true;
       midi.sendShortMsg(0x90,0x08,0x7F);
     }
