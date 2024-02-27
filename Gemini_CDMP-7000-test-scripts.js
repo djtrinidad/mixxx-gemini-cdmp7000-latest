d01b758e1c5e086d76ef5fd90487a1dc407cf3d9
djtrinidad <djtrinidad@users.noreply.github.com>
2024-02-26 22:53:08 -0600

    Update Gemini_CDMP-7000-test-scripts.js

--- Gemini_CDMP-7000-test-scripts.js
+++ Gemini_CDMP-7000-test-scripts.js
@@ -50,6 +50,24 @@ CDMP7000.Deck = function (deckNumbers, midiChannel) {
     beta: 1/8/32,
     rpm: 33 + 1/3,
 });
+
+// =================    Loop / Reloop Section     ================== //
+
+  this.loopIn = new components.Button({
+    midi: [0x90, 0x10],
+    key: "loop_in",
+  });
+
+    this.loopOut = new components.Button({
+    midi: [0x90, 0x11],
+    key: "loop_out",
+  });
+
+    this.reloopExit = new components.Button({
+    midi: [0x90, 0x12],
+    key: "reloop_exit",
+  });
+  
 // ================= Hotcue / Memo Button Section ================== //
   
   this.hotcueButtons = [];
